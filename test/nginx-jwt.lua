be26093c0e3aa1a86d4cc79289996696811dbc53
Pushpa Raj Badu <przbadu@gmail.com>
2016-01-27 14:09:24 +0545

    refactor code to remove duplication add more testcases

--- test/nginx-jwt.lua
+++ test/nginx-jwt.lua
@@ -1,70 +1,51 @@
--- local M = {}
-
--- -- USAGE:
--- --
--- --     local jwt      = require("nginx-jwt")
--- --     local table    = {}
--- --     table["roles"] = 'u.meta.admin.system'
--- --     local role     = 'u.meta'
--- --     print(table["roles"], role)
--- --     jwt.role_exists(table, 'u.meta')
--- --     true
--- --     table["roles"] = 'u.meta_admin'
--- --     role_exists(table, role)
--- --     false
--- --
--- function M.role_exists(table, role)
---   for _, value in pairs(table) do
---     if role == "u.meta" then
---       meta   = string.match(value, '^u%.meta%f[\0.]')
---       if item ==  meta then return true end
---     elseif role == "u.domain" then
---       domain = string.match(value, '^u%.domain%f[\0.]')
---       if role == domain then return true end
---     end
---   end
---   return false
--- end
-
-
-
-a = {}
-a['x'] = 'u.meta_admin'
-for _, value in pairs(a) do
-  print(_, value)
-  print('u.meta' == string.match(value, '^u%.meta%f[%z.]'))
-end
-
+function u_meta(v) return string.match(v, '^u%.meta%f[%z.]') end
+function u_domain(v) return string.match(v, '^u%.domain%f[%z.]') end
 function match_roles(table, item)
   for _, value in pairs(table) do
     if item == "u.meta" then
-      local meta = string.match(value, '^u%.meta%f[%z.]')
-      if item == meta then return true end
+      if item == u_meta(value) then return true end
     elseif item == "u.domain" then
-      local domain = string.match(value, '^u%.domain%f[%z.]')
-      if item == domain then return true end
+      if item == u_domain(value) then return true end
     end
     return false
   end
 end
 
+local t = {}
 
--- local tbl = {}
--- tbl["roles"] = 'u.meta.admin.system'
--- print(role_exists(tbl, 'u.meta'))
+-- META
+t["roles"] = 'u.meta.admin.system'
+print(match_roles(t, 'u.meta'))   -- true
 
--- tbl["roles"] = 'u.meta.admin'
--- print(role_exists(tbl, 'u.meta'))
+t["roles"] = 'u.meta.admin'
+print(match_roles(t, 'u.meta'))   -- true
 
--- tbl["roles"] = 'u.meta'
--- print(role_exists(tbl, 'u.meta'))
+t["roles"] = 'u.meta'
+print(match_roles(t, 'u.meta'))   -- true
 
--- tbl["roles"] = 'u.meta_admin'
--- print(role_exists(tbl, 'u.meta'))
+t["roles"] = 'u.meta_admin'
+print(match_roles(t, 'u.meta'))   -- false
 
--- tbl["roles"] = 'u.domain.admin'
--- print(role_exists(tbl, 'u.domain'))
+-- DOMAIN
+t['roles'] = 'u.domain'
+print(match_roles(t, 'u.domain')) -- true
 
--- tbl["roles"] = 'u.domain_admin'
--- print(role_exists(tbl, 'u.domain'))
+t["roles"] = 'u.domain.admin'
+print(match_roles(t, 'u.domain')) -- true
+
+t["roles"] = 'u.domain_admin'
+print(match_roles(t, 'u.domain')) -- false
+
+-- META try to access DOMAIN
+t["roles"] = 'u.meta'
+print(match_roles(t, 'u.domain')) -- true
+
+t["roles"] = 'u.meta.admin'
+print(match_roles(t, 'u.domain')) -- true
+
+t["roles"] = 'u.meta.admin.system'
+print(match_roles(t, 'u.domain')) -- true
+
+t["roles"] = 'u.meta_admin'
+print(match_roles(t, 'u.domain')) -- false
 
